6ba0a40fc7f8ccb25d9c745c25b8d3ba27d29744
luisEfe80 <luismendez80@hotmail.com>
2021-10-06 18:20:35 -0400

    Adición de contenido

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -10,9 +10,34 @@
     
     #t_3_1.titulo-segundo
       h2 3.1 Concepto, usos y características
+    .row.mb-5
+      .col-lg-2
+        figure(data-aos="flip-left")
+          img(src="@/assets/curso/t3-ic1.svg", alt="icono decorativo")
+      .col-lg-10
+        p Se puede inferir en la definición de las herramientas y plataformas colaborativas en línea, como un sistema en el cual se pueden acceder a muchos servicios, donde cada uno de los usuarios pueden interrelacionarse, comunicarse y trabajar en equipo, sin necesidad de estar en un mismo espacio, tiempo o lugar, todas las herramientas colaborativas brindan soluciones intrínsecas según las preferencias y perfiles de quienes las usan.
+    .row.justify-content-center
+      .col-lg-10(data-aos="fade-left")
+        .bloque-texto-a.color-acento-botones.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              figure(data-aos="flip-left")
+                img.nW(src="@/assets/curso/t3-i2.svg", alt="imagen decorativa")
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p La innovación en el mundo digital sobrepasa los límites de expansión en desarrollo de proyectos apoyados en el desarrollo tecnológico como modelo de consumo desde negocios hasta la educación, se han roto los paradigmas tradicionales de cómo te puedes relacionar o hacer tus actividades, se analizan las necesidades particulares en pro de mejorar experiencias de usuario en esferas sociales donde se busca una globalización de las cosas para beneficios comunes.
+    
+    .row.mb-5
+      .col-lg-7
+        p.mb-3(data-aos="fade-right")  Independientemente del uso que cada persona le pueda dar a las plataformas colaborativas, existe una sinergia en las interacciones desde lo social, económico, personal, profesional o personal, las cuales están diseñadas para minimizar los costos de operación y obtener muchos más beneficios, la puesta en marcha de estos nuevos modelos de compartir información toma valor en el cooperativismo por todos los actores que interactúan a través de una plataforma digital cambiando el paradigma sociocultural. Entre las más utilizadas están las siguientes plataformas colaborativas.
+        p.mb-3(data-aos="fade-right")  Los espacios virtuales centralizan sus funcionalidades a la gestión de todo tipo de información que va ligada a los requerimientos, necesidades y funcionamiento de las personas u organizaciones, en el mundo globalizado y digital; los usos que se les da, varían de acuerdo a los intereses de las personas para ser participantes activos de una comunidad, su objetivo principal radica en facilitar y optimizar la comunicación entre las personas y organizaciones generando un nuevo componente social: <b>“Tu identidad digital y el Yo Virtual”</b>, entre  las de mayor auge son:
+      .col-lg-5
+        figure(data-aos="slide-left")
+          img(src="@/assets/curso/t3-i3.jpg", alt="imagen decorativa")
 
-
-
+    .row.justify-content-center.mb-5
+      .col-lg-2
+        //-- Aqui vienen los modales
 
 
     Separador
@@ -20,6 +45,87 @@
     br
     #t_3_2.titulo-segundo
       h2 3.2 El ciudadano digital del siglo XXI
+    .row.mb-5
+      .col-lg-2
+        figure(data-aos="flip-left")
+          img(src="@/assets/curso/t3-ic2.svg", alt="icono decorativo")
+      .col-lg-10
+        p Los avances tecnológicos y la aparición de los escenarios digitales han hecho que el ciudadano digital del siglo XXI, debe tener un conglomerado de habilidades, actitudes y conocimientos, <b>para que los individuos de una sociedad puedan participar en una sociedad democrática, haciendo uso de las TIC, de forma responsable, ética, libre y participativa</b>, teniendo control sobre sus deberes y derechos digitales y llevarlas hasta sus entornos personales y sociales.
+    .row.justify-content-center
+      .col-lg-10(data-aos="fade-left")
+        .bloque-texto-a.color-acento-botones.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              figure(data-aos="flip-left")
+                img.nW(src="@/assets/curso/t3-i4.svg", alt="imagen decorativa")
+            .col-lg-8
+              .bloque-texto-a__texto.p-4.shadow-box(data-aos="fade-right")
+                p La ciudadanía digital invita a tener una mirada integradora que comprenda tanto su aprendizaje, buen uso, tu seguridad y amenazas a las cuales estás expuesto, asociados al mal uso o peligros del entorno digital, al igual que determinar de manera transparente las oportunidades ofrecen las nuevas tecnologías de información y comunicación TIC y de esta manera el ser social adquiera competencias ciudadanas para fortalecer la democracia.
+
+    p.mb-5  El uso responsable de las herramientas tecnológicas es una premisa que debe de tener el ciudadano digital del siglo XXI para poder relacionarse con sus semejantes en un tejido social, la empatía es un eje fundamental para ello, ya que con estos medios y herramientas no se tiene el contacto físico como tal se puede estar incurriendo en delitos o ser víctima de ellos, para ello, a continuación, se encuentran las diferentes dimensiones que están inmersos en ser buen ciudadano digital.
+    TabsA.color-primario.mb-5
+      .tarjeta.color-primario.p-4(titulo="Salud y bienestar digital")
+        .row
+          .col-lg-2
+            figure(data-aos="flip-left")
+              img.nW(src="@/assets/curso/t3-t1.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Salud y bienestar digital
+            p Tener cuidado sobre la salud física y mental asociado al uso prolongado de dispositivos electrónicos, la adicción y la dependencia digital es común en el día a día, por ello, se debe tener cuidado que los entornos virtuales lo saquen de la realidad.
+
+      .tarjeta.color-primario.p-4(titulo="Alfabetización digital")
+        .row
+          .col-lg-2
+            figure.slideInLeft
+              img.nW(src="@/assets/curso/t3-t2.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Alfabetización digital
+            p Conoce las nuevas herramientas tecnologías, para que se usen según las necesidades, desarrollando habilidades y competencias de su buen uso.
+
+      .tarjeta.color-primario.p-4(titulo="Seguridad digital")
+        .row
+          .col-lg-2
+            figure.slideInLeft
+              img.nW(src="@/assets/curso/t3-t3.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Seguridad digital
+            p Toma las precauciones necesarias para que la integridad no se vea en riesgo, proteja la información sensible para que terceros hagan uso indebido de la información publicada.
+
+      .tarjeta.color-primario.p-4(titulo="Acceso digital")
+        .row
+          .col-lg-2
+            figure.slideInLeft
+              img.nW(src="@/assets/curso/t3-t4.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Acceso digital
+            p Observa, comparte e interactúa de forma responsable, un ciudadano digital debe tener competencias que integran el comportamiento que se tiene con los demás.
+
+      .tarjeta.color-primario.p-4(titulo="Comunicación digital")
+        .row
+          .col-lg-2
+            figure.slideInLeft
+              img.nW(src="@/assets/curso/t3-t5.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Comunicación digital
+            p Observa, comparte e interactúa de forma responsable, un ciudadano digital debe tener competencias que integran el comportamiento que se tiene con los demás.
+
+      .tarjeta.color-primario.p-4(titulo="Comercio digital")
+        .row
+          .col-lg-2
+            figure.slideInLeft
+              img.nW(src="@/assets/curso/t3-t6.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Comercio digital
+            p Aprende a enfocarte en lo que verdaderamente se necesita comprar online, si eres vendedor es importante enfocarse en las herramientas como oportunidades no en oportunismo.
+
+      .tarjeta.color-primario.p-4(titulo="Leyes y responsabilidad digital")
+        .row
+          .col-lg-2
+            figure.slideInLeft
+              img.nW(src="@/assets/curso/t3-t7.svg", alt="icono decorativo")
+          .col-lg-10
+            .h4 Leyes y responsabilidad digital
+            p Comportarse según las normas y el uso ético de la información, el acceso a la información es ilimitado y público, úsalo de manera responsable con habilidades éticas y de autocuidado. 
 
 
 
@@ -31,8 +137,43 @@
 
     #t_3_3.titulo-segundo
       h2 3.3 Herramientas digitales de uso gubernamental, personal y empresarial
+    .row.mb-5
+      .col-lg-2
+        figure(data-aos="flip-left")
+          img(src="@/assets/curso/t3-ic3.svg", alt="icono decorativo")
+      .col-lg-10
+        p Las herramientas digitales cobijan todos los sectores ya sean públicas, personales o privadas, su uso responsable es particular dependiendo el campo de acción en las que se las use, estas incursionan de forma ágil y práctica para lograr mejor transparencia y buscan mejorar una convivencia digital efectiva. A continuación, se podrán conocer las herramientas desde el uso gubernamental, personal y empresarial.
 
+    TabsC.color-primario.mb-5
+      .py-3.py-md-4(titulo="Herramientas gubernamentales")
+        .row.justify-content-center
+          .col-md-10.mb-4.mb-md-0
+            h4 Herramientas gubernamentales
+            p.mb-4 Las estrategias de un gobierno digital o e-Gobierno radica en mejorar las comunicaciones estado - población - instituciones haciendo uso de las nuevas  tecnologías de información y comunicación y sacarles el mejor provecho en la generación de democracia, para agilizar trámites o promover la transparencia, entre las herramientas del gobierno digital te podrás encontrar con:
+            .row.justify-content-center
+              .col-lg-12
+                figure
+                  img(src='@/assets/curso/t3-info1.svg', alt='imagen decorativa')
 
+      .py-3.py-md-4(titulo="Herramientas personales")
+        .row.justify-content-center
+          .col-md-10.mb-4.mb-md-0
+            h4 Herramientas personales
+            p.mb-4 Dentro de las herramientas digitales de uso personal, el campo de acción es bastante amplio, ya que buscan ayudar a compartir información y pueda llegar a todo tipo de personas, entre las mas comunes están:
+            .row.justify-content-center
+              .col-lg-12
+                figure
+                  img(src='@/assets/curso/t3-info2.svg', alt='imagen decorativa')
+
+      .py-3.py-md-4(titulo="Herramientas empresariales")
+        .row.justify-content-center
+          .col-md-10.mb-4.mb-md-0
+            h4 Herramientas empresariales
+            p.mb-4 La empresa no es ajena a los grandes avances tecnológicos, hoy en día existen gran cantidad de herramientas que ayudan a posicionar en el mercado las empresas y mejorar su productividad, entre los cuales se puede mencionar:
+            .row.justify-content-center
+              .col-lg-12
+                figure
+                  img(src='@/assets/curso/t3-info3.svg', alt='imagen decorativa')
 
 
 
