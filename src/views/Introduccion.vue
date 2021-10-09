a9dd81f84c51bda6474d0e60a978f9a55e111082
luisEfe80 <luismendez80@hotmail.com>
2021-10-08 22:19:01 -0400

    Adicion de imagen

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,14 +8,21 @@
         span
           i.fas.fa-info
       h1 Introducción
-    
-    .corner-box.color-b1.p-4.mb-5
-      .row
-        .col-lg-3
-          //-- aqui viene la chica
-        .col-lg-9
-          p.text-xl ¿Conoces los hitos que han marcado las formas de comunicación y las diferentes transformaciones frente al comportamiento de las personas en la utilización de herramientas digitales?  Ingresa al siguiente recurso, donde te explicaremos sobre ello. 
-          //-- Aqui viene el player
+    .clearfix
+    br
+    br
+    br
+    .row
+      .col-lg-12
+        .corner-box.color-b1.p-4.mb-5
+          .row
+            .col-lg-3
+              .wrap-overflow
+                figure(data-aos="flip-left")
+                  img.nW(src="@/assets/curso/i1.png", alt="personaje animado")
+            .col-lg-9
+              p.text-xl ¿Conoces los hitos que han marcado las formas de comunicación y las diferentes transformaciones frente al comportamiento de las personas en la utilización de herramientas digitales?  Ingresa al siguiente recurso, donde te explicaremos sobre ello. 
+              //-- Aqui viene el player
 
     figure.mb-5
       .video
